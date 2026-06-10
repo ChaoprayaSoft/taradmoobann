9d4e49a5367edd5e2f3631f387ce60eab3a40f7f
ChaoprayaSoft <tiawongsombat@gmail.com>
2026-06-10 20:45:59 +0700

    Fix JSX syntax error in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -146,7 +146,6 @@ export default async function Home() {
         userEmail={userEmail} 
         spotlightProducts={spotlightProducts}
       />
-      </div>
 
       <AdsSection ads={activeAds} />
     </div>
